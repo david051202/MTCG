b882d8100c646f7de8fa36f806e06e51872fe408
david <david.froeschl8@gmail.com>
2024-10-13 15:01:03 +0200

    HTTPserver start methode

---
+++ MTCG/Http/HttpServer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MTCG.Http
+{
+    internal class HttpServer
+    {
+        private readonly TcpListener _listener;
+        private bool listerning = false;
+
+        //Starts the server
+        public HttpServer(IPAddress address, int port) {
+            _listener = new TcpListener(address, port);
+            _listener.Start();
+        }
+
+        public void Start()
+        {
+            _listener.Start();
+            listerning = true;
+
+            while (listerning)
+            {
+                var connection = _listener.AcceptTcpClient();                
+            }
+        }
+
+    }
+}
--- MTCG/Program.cs
+++ MTCG/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using MTCG.Http;
+using System.Net;
+
+HttpServer server = new HttpServer(IPAddress.Loopback, 10001);
+server.Start();
+
+Console.WriteLine("Server started on http://localhost:10001/");
