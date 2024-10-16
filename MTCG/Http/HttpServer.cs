7dc0072e7fc92bbe4988633610bf6cfbdb57c28c
davidf <david@froeschl.com>
2024-10-16 13:04:05 +0200

    Add Methodes to User class

--- MTCG/Http/HttpServer.cs
+++ MTCG/Http/HttpServer.cs
@@ -32,7 +32,59 @@ namespace MTCG.Http
                 if (line != null)
                     Console.WriteLine(line);
 
+                // 1.2 read the HTTP-headers (in HTTP after the first line, until the empy line)
+                int content_length = 0; // we need the content_length later, to be able to read the HTTP-content
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    if (line == "")
+                    {
+                        break;  // empty line indicates the end of the HTTP-headers
+                    }
+
+                    // Parse the header
+                    var parts = line.Split(':');
+                    if (parts.Length == 2 && parts[0] == "Content-Length")
+                    {
+                        content_length = int.Parse(parts[1].Trim());
+                    }
+                }
+
+                // 1.3 read the body if existing
+                if (content_length > 0)
+                {
+                    var data = new StringBuilder(200);
+                    char[] chars = new char[1024];
+                    int bytesReadTotal = 0;
+                    while (bytesReadTotal < content_length)
+                    {
+                        var bytesRead = reader.Read(chars, 0, chars.Length);
+                        bytesReadTotal += bytesRead;
+                        if (bytesRead == 0)
+                            break;
+                        data.Append(chars, 0, bytesRead);
+                    }
+                    Console.WriteLine(data.ToString());
+                }
+
+                // ----- 2. Do the processing -----
+                
+
+                Console.WriteLine("----------------------------------------");
+
+                // ----- 3. Write the HTTP-Response -----
+                var writerAlsoToConsole = new StreamTracer(writer);  // we use a simple helper-class StreamTracer to write the HTTP-Response to the client and to the console
+
+                writerAlsoToConsole.WriteLine("HTTP/1.0 200 OK");    // first line in HTTP-Response contains the HTTP-Version and the status code
+                writerAlsoToConsole.WriteLine("Content-Type: text/html; charset=utf-8");     // the HTTP-headers (in HTTP after the first line, until the empy line)
+                writerAlsoToConsole.WriteLine();
+                writerAlsoToConsole.WriteLine("<html><body><h1>Hello World!</h1></body></html>");    // the HTTP-content (here we just return a minimalistic HTML Hello-World)
+
+                Console.WriteLine("========================================");
             }
+
+
         }
+    }
     }
 }
